ba3ac97a994f82af28fe8b0cf40946404ce796e9
7SH7 <kimseunghwan7777@gmail.com>
2024-01-19 17:00:24 +0900

    #1 feat: 사용자로부터 입력받은 값 기반, 전체 틀 완성

--- src/main/java/kiosk/Application.java
+++ src/main/java/kiosk/Application.java
@@ -1,10 +1,46 @@
 package kiosk;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("프로그램 실행 시작!");
 
         startProgram();
+
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        switch(input){
+            case 1:
+                printBurgerMenu();
+            case 2:
+                printSideMenu();
+            case 3:
+                printDrinkMenu();
+            case 4:
+                printShoppingBasket();
+            case 5:
+                exitProgram();
+            default:
+                throw new IllegalArgumentException("잘못된 입력으로 인해 프로그램을 종료합니다.");
+
+        }
+
+    }
+
+    public static void printShoppingBasket() {
+    }
+
+    public static void exitProgram() {
+    }
+
+    public static void printSideMenu() {
+    }
+
+    public static void printDrinkMenu() {
+    }
+
+    public static void printBurgerMenu() {
     }
 
     public static void startProgram(){
