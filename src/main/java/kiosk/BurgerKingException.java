1e3380ef25a4377d307a82170598cfe05d9220f3
LimYeonKyuu <lim000606@naver.com>
2024-01-20 02:38:54 +0900

    edit: 파일 내부 자간 수정

--- src/main/java/kiosk/BurgerKingException.java
+++ src/main/java/kiosk/BurgerKingException.java
@@ -2,7 +2,7 @@ package kiosk;
 
 public class BurgerKingException {
   public static int checkValidationNumberInMain(int number) {
-    if (number!=1 && number!=2 && number!=3 && number!=4 && number!=5) {
+    if (number != 1 && number != 2 && number != 3 && number != 4 && number != 5) {
       throw new IllegalArgumentException("메뉴를 잘못 선택하셨습니다.");
     }
     return number;
