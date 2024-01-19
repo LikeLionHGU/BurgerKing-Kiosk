f95974e8004fc52870cf54dabafed52e85663408
7SH7 <kimseunghwan7777@gmail.com>
2024-01-19 16:40:21 +0900

    feat: startProgram() 메서드 구현

--- src/main/java/kiosk/Application.java
+++ src/main/java/kiosk/Application.java
@@ -2,7 +2,19 @@ package kiosk;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("프로그램 실행 시작!");
 
-        //TODO: 구현
+        startProgram();
     }
+
+    public static void startProgram(){
+        System.out.println("====홈====");
+        System.out.println("햄버거");
+        System.out.println("사이햐드");
+        System.out.println("음료");
+        System.out.println("장바구니");
+        System.out.println("종료");
+
+    }
+
 }
