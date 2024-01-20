fead8f67d64e078d217dec239d5c4e1812267022
7SH7 <kimseunghwan7777@gmail.com>
2024-01-20 20:27:44 +0900

    #1 refactor: Item을 생성했을 때, 기본적으로 count를 1개로 설정

--- src/main/java/kiosk/Application.java
+++ src/main/java/kiosk/Application.java
@@ -14,10 +14,10 @@ class Item{
     int price;
     int count;
 
-    Item(String itemName, int price, int count){
+    Item(String itemName, int price){
         this.itemName = itemName;
         this.price = price;
-        this.count = count;
+        this.count = 1;
     }
 }
 
@@ -125,38 +125,37 @@ public class Application {
     }
 
     public static ArrayList<Item> addBurger(ArrayList<Item> burgerArrayList){
-        burgerArrayList.add(new Item("와퍼", 6900, 0));
-        burgerArrayList.add(new Item("큐브 스테이크 와퍼", 8900, 0));
-        burgerArrayList.add(new Item("콰트로 치즈 와퍼", 7900, 0));
-        burgerArrayList.add(new Item("몬스터 와퍼", 9300, 0));
-        burgerArrayList.add(new Item("동새우 와퍼", 7900, 0));
-        burgerArrayList.add(new Item("블랙바베큐 와퍼", 9300, 0));
+        burgerArrayList.add(new Item("와퍼", 6900));
+        burgerArrayList.add(new Item("큐브 스테이크 와퍼", 8900));
+        burgerArrayList.add(new Item("콰트로 치즈 와퍼", 7900));
+        burgerArrayList.add(new Item("몬스터 와퍼", 9300));
+        burgerArrayList.add(new Item("동새우 와퍼", 7900));
+        burgerArrayList.add(new Item("블랙바베큐 와퍼", 9300));
         return burgerArrayList;
     }
 
     public static ArrayList<Item> addDrink(ArrayList<Item> drinkArrayList){
-        drinkArrayList.add(new Item("코카콜라", 2000, 0));
-        drinkArrayList.add(new Item("코카콜라 제로", 2000, 0));
-        drinkArrayList.add(new Item("펩시", 2000, 0));
-        drinkArrayList.add(new Item("펩시 제로", 2000, 0));
-        drinkArrayList.add(new Item("스프라이트", 2000, 0));
-        drinkArrayList.add(new Item("스프라이트 제로", 2000, 0));
+        drinkArrayList.add(new Item("코카콜라", 2000));
+        drinkArrayList.add(new Item("코카콜라 제로", 2000));
+        drinkArrayList.add(new Item("펩시", 2000));
+        drinkArrayList.add(new Item("펩시 제로", 2000));
+        drinkArrayList.add(new Item("스프라이트", 2000));
+        drinkArrayList.add(new Item("스프라이트 제로", 2000));
         return drinkArrayList;
     }
 
     public static ArrayList<Item> addSide(ArrayList<Item> sideArrayList){
-        sideArrayList.add(new Item("너겟킹", 2500, 0));
-        sideArrayList.add(new Item("해쉬 브라운", 1800, 0));
-        sideArrayList.add(new Item("치즈스틱", 1200, 0));
-        sideArrayList.add(new Item("어니언링", 2400, 0));
-        sideArrayList.add(new Item("바삭킹", 3000, 0));
-        sideArrayList.add(new Item("감자튀김", 2000, 0));
+        sideArrayList.add(new Item("너겟킹", 2500));
+        sideArrayList.add(new Item("해쉬 브라운", 1800));
+        sideArrayList.add(new Item("치즈스틱", 1200));
+        sideArrayList.add(new Item("어니언링", 2400));
+        sideArrayList.add(new Item("바삭킹", 3000));
+        sideArrayList.add(new Item("감자튀김", 2000));
         return sideArrayList;
     }
 
     public static void addShoppingBasket(Item food){
-        food.count++;
-        shoppingBasket.add(new Item(food.itemName, food.price, food.count));
+        shoppingBasket.add(new Item(food.itemName, food.price));
     }
 
 
@@ -189,7 +188,7 @@ public class Application {
         }
 
         if(!isSideExist) {
-            Item selectSide = new Item(side.get(selectInt-1).itemName, side.get(selectInt-1).price, side.get(selectInt-1).count);
+            Item selectSide = new Item(side.get(selectInt-1).itemName, side.get(selectInt-1).price);
             addShoppingBasket(selectSide);
         }
 
@@ -221,7 +220,7 @@ public class Application {
         }
 
         if(!isDrinkExist) {
-            Item selectSide = new Item(drink.get(selectInt-1).itemName, drink.get(selectInt-1).price, drink.get(selectInt-1).count);
+            Item selectSide = new Item(drink.get(selectInt-1).itemName, drink.get(selectInt-1).price);
             addShoppingBasket(selectSide);
         }
 
@@ -257,7 +256,7 @@ public class Application {
         }
 
         if(!isBurgerExist) {
-            Item selectSide = new Item(burger.get(selectInt-1).itemName, burger.get(selectInt-1).price, burger.get(selectInt-1).count);
+            Item selectSide = new Item(burger.get(selectInt-1).itemName, burger.get(selectInt-1).price);
             addShoppingBasket(selectSide);
         }
 
